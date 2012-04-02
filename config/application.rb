881ef25a66c0afae85abbd2f184b43e0dc87179e
Tim Kinisky <tim.kinisky@gmail.com>
2012-04-01 22:05:12 -0700

    Done with static pages

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -61,5 +62,12 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    ### Part of a Spork hack. See http://bit.ly/arY19y
+    if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+    # Work around initializer in railties/lib/rails/application/bootstrap.rb 
+    ActiveSupport::Dependencies.mechanism = :load
+    end end
   end
 end
